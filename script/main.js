1d0684b0fb2ad31da119a3b95b30b9fb5068d277
赛义甫 <dunai2004@hotmail.com>
2022-05-22 21:33:44 -0600

    add css and script folders and update index.html

---
+++ script/main.js
@@ -0,0 +1,7 @@
+  date = new Date();
+  year = date.getFullYear();
+  month = date.getMonth() + 1;
+  day = date.getDate();
+  document.getElementById("now").innerHTML = "今天是" + year +"年" + month + "月" + day + "日";
+
+
