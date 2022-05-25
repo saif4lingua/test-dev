d636074eba36e8731403c90e2980a86715e2a838
赛义甫 <dunai2004@hotmail.com>
2022-05-25 10:39:47 -0600

    changed css to style, added template.html, post.css, updated index.html, main.css blog page and post page.

--- script/main.js
+++ script/main.js
@@ -4,4 +4,26 @@
   day = date.getDate();
   document.getElementById("now").innerHTML = "今天是" + year +"年" + month + "月" + day + "日";
 
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementByTagName("*");
+  for (i=0; i<z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page Not Found.");}
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        } /* end inner if*/
+      } /* end inner function */
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    } /* end outer if */
+  }
+};
 
